04129e7b5239073e74df8144c9138a49055ae087
ravi singh <ravisinghofficial@gmail.com>
2019-02-26 18:51:29 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Package to upload logs on logstash
---
+++ drf_tracking/mixins.py
@@ -0,0 +1,26 @@
+from .base_mixins import BaseLoggingMixin
+import logging
+
+logstash_logger = logging.getLogger('logstash-logger')
+
+
+class LoggingMixin(BaseLoggingMixin):
+    def handle_log(self):
+        """
+        Hook to define what happens with the log.
+
+        Defaults on saving the data on the db.
+        """
+        if self.log['status_code'] == 200:
+            logstash_logger.info('Success response', extra=self.log)
+        else:
+            logstash_logger.info('Error response', extra=self.log)
+
+
+class LoggingErrorsMixin(LoggingMixin):
+    """
+    Log only errors
+    """
+
+    def should_log(self, request, response):
+        return response.status_code >= 400
