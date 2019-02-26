5a9bcdb151733a7e10ae83c492ec48764c56bff8
ravi singh <ravisinghofficial@gmail.com>
2019-02-26 19:12:30 +0530

    pypi setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='drf_tracking_logstash',
+    version='0.1',
+    author="Ravi Singh",
+    author_email="ravisinghofficial@gmail.com",
+    description="Log requests on logstash",
+    url="https://github.com/singhravi1/drf-tracking-logstash",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
